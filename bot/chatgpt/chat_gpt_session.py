10369aa7e9bfac84daee5f2423f2493aff225f08
FB208 <yhc_fb@sina.com>
2024-06-28 14:55:08 +0800

    不在校验用了多少token（为了兼容第三方接口）

--- bot/chatgpt/chat_gpt_session.py
+++ bot/chatgpt/chat_gpt_session.py
@@ -56,44 +56,44 @@ class ChatGPTSession(Session):
 # refer to https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
 def num_tokens_from_messages(messages, model):
     """Returns the number of tokens used by a list of messages."""
+    return 10
+    # if model in ["wenxin", "xunfei", const.GEMINI]:
+    #     return num_tokens_by_character(messages)
 
-    if model in ["wenxin", "xunfei", const.GEMINI]:
-        return num_tokens_by_character(messages)
+    # import tiktoken
 
-    import tiktoken
-
-    if model in ["gpt-3.5-turbo-0301", "gpt-35-turbo", "gpt-3.5-turbo-1106", "moonshot", const.LINKAI_35]:
-        return num_tokens_from_messages(messages, model="gpt-3.5-turbo")
-    elif model in ["gpt-4-0314", "gpt-4-0613", "gpt-4-32k", "gpt-4-32k-0613", "gpt-3.5-turbo-0613",
-                   "gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613", "gpt-35-turbo-16k", "gpt-4-turbo-preview",
-                   "gpt-4-1106-preview", const.GPT4_TURBO_PREVIEW, const.GPT4_VISION_PREVIEW, const.GPT4_TURBO_01_25,
-                   const.GPT_4o, const.LINKAI_4o, const.LINKAI_4_TURBO]:
-        return num_tokens_from_messages(messages, model="gpt-4")
-    elif model.startswith("claude-3"):
-        return num_tokens_from_messages(messages, model="gpt-3.5-turbo")
-    try:
-        encoding = tiktoken.encoding_for_model(model)
-    except KeyError:
-        logger.debug("Warning: model not found. Using cl100k_base encoding.")
-        encoding = tiktoken.get_encoding("cl100k_base")
-    if model == "gpt-3.5-turbo":
-        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
-        tokens_per_name = -1  # if there's a name, the role is omitted
-    elif model == "gpt-4":
-        tokens_per_message = 3
-        tokens_per_name = 1
-    else:
-        logger.warn(f"num_tokens_from_messages() is not implemented for model {model}. Returning num tokens assuming gpt-3.5-turbo.")
-        return num_tokens_from_messages(messages, model="gpt-3.5-turbo")
-    num_tokens = 0
-    for message in messages:
-        num_tokens += tokens_per_message
-        for key, value in message.items():
-            num_tokens += len(encoding.encode(value))
-            if key == "name":
-                num_tokens += tokens_per_name
-    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
-    return num_tokens
+    # if model in ["gpt-3.5-turbo-0301", "gpt-35-turbo", "gpt-3.5-turbo-1106", "moonshot", const.LINKAI_35]:
+    #     return num_tokens_from_messages(messages, model="gpt-3.5-turbo")
+    # elif model in ["gpt-4-0314", "gpt-4-0613", "gpt-4-32k", "gpt-4-32k-0613", "gpt-3.5-turbo-0613",
+    #                "gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613", "gpt-35-turbo-16k", "gpt-4-turbo-preview",
+    #                "gpt-4-1106-preview", const.GPT4_TURBO_PREVIEW, const.GPT4_VISION_PREVIEW, const.GPT4_TURBO_01_25,
+    #                const.GPT_4o, const.LINKAI_4o, const.LINKAI_4_TURBO]:
+    #     return num_tokens_from_messages(messages, model="gpt-4")
+    # elif model.startswith("claude-3"):
+    #     return num_tokens_from_messages(messages, model="gpt-3.5-turbo")
+    # try:
+    #     encoding = tiktoken.encoding_for_model(model)
+    # except KeyError:
+    #     logger.debug("Warning: model not found. Using cl100k_base encoding.")
+    #     encoding = tiktoken.get_encoding("cl100k_base")
+    # if model == "gpt-3.5-turbo":
+    #     tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+    #     tokens_per_name = -1  # if there's a name, the role is omitted
+    # elif model == "gpt-4":
+    #     tokens_per_message = 3
+    #     tokens_per_name = 1
+    # else:
+    #     logger.warn(f"num_tokens_from_messages() is not implemented for model {model}. Returning num tokens assuming gpt-3.5-turbo.")
+    #     return num_tokens_from_messages(messages, model="gpt-3.5-turbo")
+    # num_tokens = 0
+    # for message in messages:
+    #     num_tokens += tokens_per_message
+    #     for key, value in message.items():
+    #         num_tokens += len(encoding.encode(value))
+    #         if key == "name":
+    #             num_tokens += tokens_per_name
+    # num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    # return num_tokens
 
 
 def num_tokens_by_character(messages):
